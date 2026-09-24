44cf35ff52a62cfc1a5fdbfd7f402195fb0c6952
AGN200486 <vellaagustin04@gmail.com>
2026-09-24 16:27:31 -0300

    agregacion del archivo gitignore en la carpeta raiz

--- src/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0235179c37c46e94cc114b333a06652e5b0160e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a7351dc85e3b688488bb47b9bc1464aac9dbfe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
